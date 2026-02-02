4977d3f9ccfaea907697227311148384659bf41d
abdullah7175 <abdullah7175@gmail.com>
2026-02-03 01:55:07 +0500

    comit

--- src/pages/My Orders.bnfwo.js
+++ src/pages/My Orders.bnfwo.js
@@ -1,6 +1,6 @@
 // Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 
-$w.onReady(function () {
+// $w.onReady(function () {
 
 	// Write your Javascript code here using the Velo framework API
 
@@ -12,4 +12,29 @@ $w.onReady(function () {
 
 	// Click "Run", or Preview your site, to execute your code
 
-});
+// });
+import wixData from 'wix-data';
+
+export async function getAllItems(nameCollection, limit) {
+
+    let options = {
+        "suppressAuth": true
+    };
+
+    let results = await wixData.query(nameCollection)
+        .limit(limit)
+        .find(options);
+    let allItems = results.items;
+    while (results.hasNext()) {
+        results = await results.next();
+        allItems = allItems.concat(results.items);
+    }
+
+    return allItems;
+}
+
+export function getOrders() {
+    let products = getAllItems("Stores/Orders", 100);
+
+    return products;
+}
